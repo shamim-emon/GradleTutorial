71aa7d49c5c7922bf4f550bd98fcfd5436f3cc92
shamim-emon <emon.dev.bd@gmail.com>
2023-01-27 18:42:33 +0600

    Initial Set-up

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories.gradlePluginPortal()
+    //repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.localtion/repo"){
+//        credentials.username ="...."
+//        credentials.password ="...."
+//    }
+//
+//    includeBuild("gradle/plugins")
+}
+dependencyResolutionManagement {
+    repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.localtion/repo"){
+//        credentials.username ="...."
+//        credentials.password ="...."
+//    }
+//
+//    includeBuild("../my-other-project")
+}
+
+rootProject.name = "GradleTutorial"
+
+include("app")
+include("business-logic")
+include("data-model")
